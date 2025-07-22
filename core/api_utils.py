57ff7d7d76353bf6475af5c043e293329808acf7
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-07-21 18:43:00 -0600

    added starter for api_utils. CFC implemented.

---
+++ core/api_utils.py
@@ -0,0 +1,93 @@
+"utilities for talking with CFC API"""
+# Copyright (C) 2024  Nicolas Vaagen
+#
+# This program is free software.
+
+import httpx
+
+from .utils import root_logger
+
+URL_STUB = "https://server.chess.ca"
+
+
+def get_cfc_player_info(cfc_id: str):
+    """Retrieve player information from CFC API.
+
+    Gets player details including name, rating and membership status
+    from the Chess Federation of Canada API.
+
+    Parameters
+    ----------
+    cfc_id : str
+        The CFC ID number of the player to look up.
+
+    Returns
+    -------
+    dict - JSON response from the API.
+        example:
+        {'updated': '2025-04-24',
+        'player': {
+            'cfc_id': 123123,
+            'cfc_expiry': '2020-01-02',
+            'fide_id': 0,
+            'name_first': 'Michael',
+            'name_last': 'Williams',
+            'addr_city': "St.John's",
+            'addr_province': 'NL',
+            'regular_rating': 200,
+            'regular_indicator': 11,
+            'quick_rating': 200,
+            'quick_indicator': 11,
+            'events': [{'id': 199806005,
+                'name': 'MacDonald Dr RR',
+                'date_end': '1998-05-14',
+                'rating_type': 'R',
+                'games_played': 11,
+                'score': 0.0,
+                'rating_pre': 0,
+                'rating_perf': 0,
+                'rating_post': 200,
+                'rating_indicator': 11}],
+            'orgarb': [],
+            'is_organizer': False,
+            'is_arbiter': False
+        },
+        'apicode': 0,
+        'error': ''}
+
+        Player information from the API response.
+        Contains fields like name, rating, expiry date, etc.
+
+    Raises
+    ------
+    httpx.exceptions.RequestException
+        If the API request fails for any reason
+    ValueError
+        If the response is not valid JSON
+    """
+    api_path = f"/api/player/v1/{cfc_id}"
+
+    try:
+        r = httpx.get(URL_STUB + api_path)
+        r.raise_for_status()
+
+        api_info = r.json()
+        root_logger.debug("CFC API response: %s", api_info)
+
+        if "name_last" not in api_info["player"]:
+            raise ValueError(f"{api_path} did not return a player")
+
+        return api_info["player"]
+
+    except httpx.HTTPError as e:
+        raise httpx.HTTPError(
+            f"HTTP error: {e}")
+
+
+def get_uscf_player_info(uscf_id):
+    """Get the player info from uscf API"""
+    raise NotImplementedError()
+
+def get_fide_player_info(fide_id):
+    """Get the player info from fide API"""
+    raise NotImplementedError()
