aa6ab89c663cca0cce37726d87effff20d7ccaff
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-07-24 14:50:49 -0600

    revert setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,51 @@
 import sys
-from distutils.core import setup
+from cx_Freeze import setup, Executable
+
+# GUI vs console base
+base = "Win32GUI" if sys.platform == "win32" else None
+
+# Include entire folders along with other files
+build_exe_options = {
+    "packages": ["requests", "packaging"],
+    "include_files": [
+        "styles.qss",
+        ("resources/icons/icon.ico", "icon.ico"), # Correctly reference and place the icon
+        "license.rtf",
+        "version.json",
+        "core/",       # include entire core folder
+        "gui/",        # include entire gui folder
+        "resources/",  # include entire resources folder
+    ],
+}
+
+# MSI installer options
+bdist_msi_options = {
+    "upgrade_code": "{E7CDA630-CB74-4A99-BDB4-F09CB777D3F8}",
+    "add_to_path": False,
+    "initial_target_dir": r"[ProgramFilesFolder]\Gambit Pairing",
+    "install_icon": "icon.ico", # This now correctly points to the included icon
+    "launch_on_finish": True,
+    "license_file": "license.rtf",
+}
+
+executables = [
+    Executable(
+        script="gambitpairing/__main__.py",
+        base=base,
+        icon="icon.ico",
+        target_name="GambitPairing.exe",
+        shortcut_name="Gambit Pairing",
+        shortcut_dir="DesktopFolder"
+    )
+]
 
 setup(
-    name="GambitPairing",
-    version="0.5.0",
-    description="Gambit Pairing",
-    long_description=open("README.md").read(),
-    packages=["gambitpairing"]
+    name="Gambit Pairing",
+    version="0.4.0",
+    description="Swiss pairing app to streamline tournament management.",
+    options={
+        "build_exe": build_exe_options,
+        "bdist_msi": bdist_msi_options
+    },
+    executables=executables
 )
