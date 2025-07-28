f713066d9bd45f900d3b5893acf489b86ba479e1
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-07-28 10:04:18 -0600

    doc fixup

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ cd $SCRIPT_DIR
 python -m pip install .
 
 echo "gambit-pairing is now installed as a gui script"
+echo "try running gambit-pairing from a shell in the python environment you installed in"
