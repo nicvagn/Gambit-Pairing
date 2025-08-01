f860c2469dfe840112b7514285c48572e03a4440
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-01 09:38:19 -0600

    more script mess about. I think it works now

--- launch_from_src.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "running from the src directory."
-trap 'echo "FAIL: Make sure you have installed pip requirements, and activated any relevant venvs"' ERR
-SCRIPT_DIR=$(dirname "$0")
-python3 $SCRIPT_DIR/src/gambitpairing
