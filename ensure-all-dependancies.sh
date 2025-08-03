aa01a518bcc9b01e8ca234c26356287349872d2e
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-03 14:07:44 -0600

    ensure pip when installing packages

--- ensure-all-dependancies.sh
+++ ensure-all-dependancies.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-
+echo "ensuring pip is up to date"
+python -m ensurepip --upgrade
 echo "installing dependancise"
 python -m pip install $(python -c 'import tomllib; print(*tomllib.load(open("pyproject.toml", "rb"))["project"]["dependencies"])')
 
