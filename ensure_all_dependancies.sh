661654d69ddbd031af3ce26711df470c254c8e65
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-05 20:46:05 -0600

    make all commands use python3

--- ensure_all_dependancies.sh
+++ ensure_all_dependancies.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 echo "ensuring pip is up to date"
-python -m ensurepip --upgrade
+python3 -m ensurepip --upgrade
 echo "installing dependancise"
-python -m pip install $(python -c 'import tomllib; print(*tomllib.load(open("pyproject.toml", "rb"))["project"]["dependencies"])')
+python3 -m pip install $(python3 -c 'import tomllib; print(*tomllib.load(open("pyproject.toml", "rb"))["project"]["dependencies"])')
 
 echo "installing dev dependancise"
-python -m pip install $(python -c 'import tomllib; print(*tomllib.load(open("pyproject.toml", "rb"))["project"]["optional-dependencies"]["dev"])')
+python3 -m pip install $(python3 -c 'import tomllib; print(*tomllib.load(open("pyproject.toml", "rb"))["project"]["optional-dependencies"]["dev"])')
