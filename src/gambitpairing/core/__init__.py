7aae90b310286063c54c50b5917475d758b89a03
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-06 16:25:29 -0600

    Pairing Round Robin logic 1.0
    also added some helper types

--- src/gambitpairing/core/__init__.py
+++ src/gambitpairing/core/__init__.py
@@ -3,6 +3,10 @@
 from typing import List, Tuple, Optional, Set, Dict, Any
 from gambitpairing.core.player import Player
 
-type Pairings = Tuple[List[Tuple[Player, Player]], Optional[Player]]
-type MaybePlayer = Optional[Player]
-type Players = List[Players]
+RoundSchedule = Tuple[MatchPairing, ...]  # All pairings for one round
+MatchPairing = Tuple[int, int]  # Tuple of player indices
+Pairings = Tuple[List[Tuple[Player, Player]], Optional[Player]]
+MaybePlayer = Optional[Player]
+Players = List[Player]
+
+#  LocalWords:  MatchPairing RoundSchedule
