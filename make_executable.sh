98985d3d872786292ba0ffd12f3f2309fce9f451
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-05 20:42:01 -0600

    made make_executable.sh cleaner

--- make_executable.sh
+++ make_executable.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
-
 SCRIPT_DIR=$(dirname "$0")
-
-pip install pyinstaller
-
-pyinstaller $SCRIPT_DIR/Gambit-Pairing.spec
+pip install pyinstaller || exit 1
+pyinstaller "$SCRIPT_DIR/Gambit-Pairing.spec"
