a8de14018c85b32a572e30efda6396d6be89a22a
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-06 17:07:46 -0600

    added comprehensive tests for Round Robin Pairing

--- src/gambitpairing/core/__init__.py
+++ src/gambitpairing/core/__init__.py
@@ -3,8 +3,8 @@
 from typing import List, Tuple, Optional, Set, Dict, Any
 from gambitpairing.core.player import Player
 
-RoundSchedule = Tuple[MatchPairing, ...]  # All pairings for one round
 MatchPairing = Tuple[int, int]  # Tuple of player indices
+RoundSchedule = Tuple[MatchPairing, ...]  # All pairings for one round
 Pairings = Tuple[List[Tuple[Player, Player]], Optional[Player]]
 MaybePlayer = Optional[Player]
 Players = List[Player]
