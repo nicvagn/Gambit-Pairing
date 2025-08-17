e7f99e1fe460b058c2f218943afd414ed036c304
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-17 10:09:14 -0600

    made tournament settings dialog spread out and more explicitly named

--- src/gambitpairing/gui/dialogs/__init__.py
+++ src/gambitpairing/gui/dialogs/__init__.py
@@ -20,7 +20,7 @@ from .about_dialog import AboutDialog
 from .manual_pairing_dialog import ManualPairingDialog
 from .new_tournament_dialog import NewTournamentDialog
 from .player_management_dialog import PlayerManagementDialog
-from .settings_dialog import SettingsDialog
+from .tournament_settings_dialoug import SettingsDialog
 from .update_dialog import UpdateDownloadDialog
 from .update_prompt_dialog import UpdatePromptDialog
 
