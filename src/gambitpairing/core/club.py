06ab74c63296eee4e579ff1b489401c6be22e62e
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-08-20 15:36:54 -0600

    Reapply "Started on breaking player into base and super class."
    This reverts commit 5c8b47d30c57e8929144738402724770fbd8a422.

---
+++ src/gambitpairing/core/club.py
@@ -0,0 +1,30 @@
+"""Chess club container class."""
+
+# Gambit Pairing
+# Copyright (C) 2025  Gambit Pairing developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class Club:
+    """A chess club.
+
+    Attributes
+    ----------
+    name : str
+    """
+
+    def __init__(self, name: str) -> None:
+        """Set the created club name."""
+        self.name = name
